7c2ee124fc2dbbeaf264cd6ada770ba1200988cd
lukcaster <gruchala.lukasz1@gmail.com>
2019-10-12 09:14:40 +0200

    zadanie

---
+++ src/test/java/przyklad1/SearchTest.java
@@ -0,0 +1,66 @@
+package przyklad1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.junit.*;
+import java.util.Random;
+
+public class SearchTest {
+
+    private WebDriver driver;
+    String[] products = {"mug", "notebook", "cushion", "t-shirt", "sweater"};
+    Random random = new Random();
+    int randomInteger = random.nextInt(5);
+    @Before
+    public void setUp() {
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        // Zmaksymalizuj okno przeglądarki
+        driver.manage().window().maximize();
+        // Przejdź do Strony coderslab
+        driver.get("https://prod-kurs.coderslab.pl");
+    }
+    @Test
+    public void testGoogleSearch() {
+        // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+        WebElement element = driver.findElement(By.name("s"));
+        // Wyczyść teskst zapisany w elemencie
+        element.clear();
+
+        // Wpisz informacje do wyszukania
+        element.sendKeys(products[randomInteger]);
+        String x ="";
+        driver.findElements(By.id(x));
+        boolean tak = true;
+        boolean nie = false;
+
+            if (x = "mug");
+        {
+
+            assert (driver.findElement(By.id("19")).equals("Wyszukiwarka dziala dobrze"));
+        }
+        else  if(x ="notebook"); {
+            assert (driver.findElement(By.id("16")).equals("Wyszukiwarka dziala dobrze"));
+        }
+        else  if(x ="cushion"); {
+            assert (driver.findElement(By.id("10")).equals("Wyszukiwarka dziala dobrze"));
+        }
+        else if(x ="t-shirt"); {
+            assert (driver.findElement(By.id("1")).equals("Wyszukiwarka dziala dobrze"));
+        }
+        else (x ="sweater"); {
+            assert (driver.findElement(By.id("2")).equals("Wyszukiwarka dziala dobrze"));
+        }
+        element.submit();
+    }
+    @After
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+        //driver.quit();
+    }
+}
